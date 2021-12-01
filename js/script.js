64f3da343396bad314397fe191ce00fd0f8541c5
edoinjp <edoinjp@gmail.com>
2021-12-01 17:58:21 +0400

    burger update

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let navigation = document.getElementById("navv")
+let burgeticon = document.getElementById("burgericon")
+
+burgeticon.addEventListener("click", function(){
+    navigation.classList.toggle('ul-active')
+})
+
